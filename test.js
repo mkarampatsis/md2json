4de7199de18e3feacc04744b70c47c7324287dd8
mkarampatsis <marka@central.ntua.gr>
2022-12-04 22:28:20 +0200

    Initialize project

--- test.js
+++
@@ -1,5 +0,0 @@
-const querySelectorAll = require('query-selector');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
